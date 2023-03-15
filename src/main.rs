d42ddcff58f47e4b2066513f52817ec6786f91a7
Nyctef <nyctef@nyctef.com>
2023-03-15 20:25:35 +0000

    use map() on stream instead of manual loop
    currently this changes the return type so that the stream is always
    exhausted/collected before returning, but we could potentially reverse
    that if streaming ends up being important again

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::collections::HashMap;
 use color_eyre::eyre::Result;
 use model::{ItemId, Recipe, RecipeId, RecipeItem};
 use tokio::fs::File;
-use tokio_stream::{self as stream, Stream, StreamExt};
+use tokio_stream::StreamExt;
 
 mod model;
 
-async fn read_recipes(file_in: &str) -> Result<impl Stream<Item = Recipe>> {
+async fn read_recipes(file_in: &str) -> Result<Vec<Recipe>> {
     // Function reads CSV file that has column named "region" at second position (index = 1).
     // It writes to new file only rows with region equal to passed argument
     // and removes region column.
@@ -28,37 +28,33 @@ async fn read_recipes(file_in: &str) -> Result<impl Stream<Item = Recipe>> {
     let types_row = records.next().await.expect("types row")?;
     // map from a field index to the type of that field
     let types_lookup = types_row.iter().enumerate().collect::<HashMap<_, _>>();
-    let mut result = vec![];
-    let mut records = records.take(10);
 
-    while let Some(record) = records.next().await {
-        let record = record?;
-        // result.push(format!(
-        //     "id: {} result: {}",
-        //     record.get(*field_name_lookup.get("#").unwrap()).unwrap(),
-        //     record
-        //         .get(*field_name_lookup.get("Item{Result}").unwrap())
-        //         .unwrap(),
-        // ));
-        let recipe_id = record.get(*field_name_lookup.get("#").unwrap()).unwrap();
-        let mut items = vec![];
-        for i in 0..10 {
-            let ingredient_field_name: &str = &format!("Item{{Ingredient}}[{i}]");
-            let ingredient_id = record
-                .get(*field_name_lookup.get(ingredient_field_name).unwrap())
-                .unwrap();
-            let amount_field_name: &str = &format!("Amount{{Ingredient}}[{i}]");
-            let amount: u8 = record
-                .get(*field_name_lookup.get(amount_field_name).unwrap())
-                .unwrap()
-                .parse()?;
-            if amount > 0 {
-                items.push(RecipeItem::new(ItemId::try_from(ingredient_id)?, amount))
+    let result = records
+        .map(|record| {
+            let record = record?;
+
+            let recipe_id = record.get(*field_name_lookup.get("#").unwrap()).unwrap();
+            let mut items = vec![];
+            for i in 0..10 {
+                let ingredient_field_name: &str = &format!("Item{{Ingredient}}[{i}]");
+                let ingredient_id = record
+                    .get(*field_name_lookup.get(ingredient_field_name).unwrap())
+                    .unwrap();
+                let amount_field_name: &str = &format!("Amount{{Ingredient}}[{i}]");
+                let amount: u8 = record
+                    .get(*field_name_lookup.get(amount_field_name).unwrap())
+                    .unwrap()
+                    .parse()?;
+                if amount > 0 {
+                    items.push(RecipeItem::new(ItemId::try_from(ingredient_id)?, amount))
+                }
             }
-        }
-        result.push(Recipe::new(RecipeId::try_from(recipe_id)?, items));
-    }
-    Ok(stream::iter(result))
+            Ok(Recipe::new(RecipeId::try_from(recipe_id)?, items))
+        })
+        .collect()
+        .await;
+
+    result
 }
 
 #[tokio::main]
@@ -66,8 +62,9 @@ async fn main() -> Result<()> {
     color_eyre::install()?;
 
     let result = read_recipes("../ffxiv-datamining/csv/Recipe.csv").await?;
+    let result = result.iter().take(10);
 
-    dbg!(result.collect::<Vec<_>>().await);
+    dbg!(result.collect::<Vec<_>>());
 
     Ok(())
 }
