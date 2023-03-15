e74e4bdeac1a885489ce8ff6fd0871b7972d5563
Nyctef <nyctef@nyctef.com>
2023-03-15 17:55:43 +0000

    start reading from ff14 csv dump

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,32 @@
-use std::error::Error;
-use std::process;
 use tokio_stream::StreamExt;
 use tokio::fs::File;
+use color_eyre::eyre::Result;
 
-async fn filter_by_region(region:&str, file_in:&str, file_out:&str) -> Result<(), Box<dyn Error>> {
+async fn print_ten_rows(file_in:&str) -> Result<Vec<String>> {
     // Function reads CSV file that has column named "region" at second position (index = 1).
     // It writes to new file only rows with region equal to passed argument
     // and removes region column.
-    let mut rdr = csv_async::AsyncReader::from_reader(
+    let mut reader = csv_async::AsyncReader::from_reader(
         File::open(file_in).await?
     );
-    let mut wri = csv_async::AsyncWriter::from_writer(
-        File::create(file_out).await?
-    );
-    wri.write_record(rdr
-        .headers()
-        .await?.into_iter()
-        .filter(|h| *h != "region")
-    ).await?;
-    let mut records = rdr.records();
+    let mut records = reader.records().take(10);
+    let mut result = vec![];
     while let Some(record) = records.next().await {
         let record = record?;
-        match record.get(1) {
-            Some(reg) if reg == region => 
-                wri.write_record(record
-                    .iter()
-                    .enumerate()
-                    .filter(|(i, _)| *i != 1)
-                    .map(|(_, s)| s)
-                ).await?,
-            _ => {},
-        }
+        result.push(record.iter().map(|x| x.to_owned()).collect::<Vec<_>>().join("|"));
     }
-    Ok(())
+    Ok(result)
 }
 
 #[tokio::main]
-async fn main() {
-    if let Err(err) = filter_by_region(
-        "MA",
-        "/tmp/all_regions.csv",
-        "/tmp/MA_only.csv"
-    ).await {
-        eprintln!("error running filter_by_region: {}", err);
-        process::exit(1);
-    }
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let result = print_ten_rows(
+        "../ffxiv-datamining/csv/Recipe.csv"
+    ).await?;
+
+    dbg!(result);
+
+    Ok(())
 }
