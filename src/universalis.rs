6f963fbf318e6bbb4be646f8f14281b6f0040f43
Nyctef <nyctef@nyctef.com>
2023-03-18 21:54:10 +0000

    start being a little smarter than just reporting the cheapest value

--- src/universalis.rs
+++ src/universalis.rs
@@ -1,11 +1,11 @@
 use crate::model::*;
 use chrono::{DateTime, TimeZone, Utc};
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
 use derive_more::Constructor;
 use itertools::Itertools;
 use reqwest::Client;
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::{cmp, collections::HashMap};
 
 #[derive(Debug, Constructor)]
 pub struct ItemMarketData {
@@ -105,4 +105,70 @@ pub async fn get_market_data(ids: impl Into<&[ItemId]>) -> Result<Vec<ItemMarket
             )
         })
         .collect_vec())
+}
+
+/// Note this is relatively naive: it assumes the latest universalis data is still valid +
+/// it assumes that we'll happily buy stacks of up to 99 items even if we wanted less and
+/// can easily resell the remainder. It also assumes that the incoming listings are sorted
+/// by price (which seems to always be the case from what the universalis api gives us?)
+fn price_up_to(
+    listings: &[ItemMarketListing],
+    amount_wanted: u32,
+    hq_only: bool,
+) -> Result<u32, String> {
+    let mut amount_remaining = amount_wanted;
+    let mut cumulative_price = 0;
+    let mut listings = listings
+        .iter()
+        // https://stackoverflow.com/a/68522183
+        .filter(|l| if hq_only { l.hq } else { true });
+
+    while amount_remaining > 0 {
+        match listings.next() {
+            None => break,
+            Some(next_listing) => {
+                cumulative_price +=
+                    next_listing.price_per_item * (cmp::min(amount_remaining, next_listing.amount));
+                amount_remaining = amount_remaining.saturating_sub(next_listing.amount);
+            }
+        }
+    }
+    match amount_remaining {
+        x if x > 0 => Err(format!("Couldn't find enough listings to satisfy demand")),
+        _ => Ok(cumulative_price),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_price_up_to() {
+        let listings = vec![
+            ItemMarketListing::new(10, 1, false),
+            ItemMarketListing::new(20, 1, true),
+            ItemMarketListing::new(100, 10, false),
+        ];
+
+        // just buying the cheapest
+        assert_eq!(Ok(10), price_up_to(&listings, 1, false));
+
+        // buying the two cheapest
+        assert_eq!(Ok(30), price_up_to(&listings, 2, false));
+
+        // buying three cuts into the big stack
+        // but we don't report the price of having to buy the whole thing
+        assert_eq!(Ok(130), price_up_to(&listings, 3, false));
+
+        // buying only HQ means we can't buy the cheapest
+        assert_eq!(Ok(20), price_up_to(&listings, 1, true));
+
+        // trying to buy more than is available
+        assert_eq!(
+            Err("Couldn't find enough listings to satisfy demand".to_owned()),
+            price_up_to(&listings, 20, false)
+        );
+    }
 }
