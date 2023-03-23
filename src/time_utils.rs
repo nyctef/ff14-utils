dc711c6d5cab2a2041a90cc88af9f8948e9f99f5
Nyctef <nyctef@nyctef.com>
2023-03-23 14:46:59 +0000

    hm_ago can now show seconds in the edge case of <1m

--- src/time_utils.rs
+++ src/time_utils.rs
@@ -1,8 +1,9 @@
 use chrono::{DateTime, Duration, Utc};
 
 pub fn hm_ago(dur: Duration) -> String {
-    // xxm ago for durations < 1h, yyhxxm ago for durations >= 1h
-    if dur.num_minutes() < 60 {
+    if dur.num_minutes() < 1 {
+        format!("{}s ago", dur.num_seconds())
+    } else if dur.num_minutes() < 60 {
         format!("{}m ago", dur.num_minutes())
     } else {
         format!("{}h{}m ago", dur.num_hours(), dur.num_minutes() % 60)
