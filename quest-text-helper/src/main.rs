4d79fe7b02893406fbd5c899038614dd2fdbbb0d
Nyctef <nyctef@nyctef.com>
2023-03-28 17:36:10 +0100

    add context for error handling in a couple of cases

--- quest-text-helper/src/main.rs
+++ quest-text-helper/src/main.rs
@@ -1,4 +1,4 @@
-use color_eyre::eyre::{eyre, Result};
+use color_eyre::eyre::{eyre, Context, Result};
 use grep::{
     self,
     regex::RegexMatcher,
@@ -20,15 +20,20 @@ fn main() -> Result<()> {
         _ => return Err(eyre!("Usage: quest-text-helper [path to ACT logs folder]")),
     };
 
-    let folder_files: Result<Vec<_>> = fs::read_dir(folder)?
+    let folder_files: Result<Vec<_>> = fs::read_dir(folder)
+        .wrap_err_with(|| format!("Failed to read files from folder: {folder}"))?
         .map(|f| -> Result<(PathBuf, SystemTime)> {
             let f = f?;
             Ok((f.path(), f.metadata()?.modified()?))
         })
         .collect();
+
     let mut folder_files = folder_files?;
     folder_files.sort_by_key(|f| f.1);
-    let newest_file = folder_files.pop().unwrap().0;
+    let newest_file = folder_files
+        .pop()
+        .ok_or_else(|| eyre!("Folder {folder} appears to be empty"))?
+        .0;
     dbg!(&newest_file);
 
     let pattern = "\\|003D\\|";
