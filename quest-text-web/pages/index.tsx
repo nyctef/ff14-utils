53308f5eab6ef5f08bfe33c010ac9c906bdd278b
Nyctef <nyctef@nyctef.com>
2023-04-18 15:17:41 +0100

    start getting some styles working

--- quest-text-web/pages/index.tsx
+++ quest-text-web/pages/index.tsx
@@ -3,6 +3,13 @@ import React from "react";
 function HomePage() {
   return (
     <>
+      <style jsx global>{`
+      body {
+        background-color: #555;
+        color: #eee;
+        font-family: Verdana, Arial, sans-serif;
+        }
+      `}</style>
       <div>test2</div>
     </>
   );
