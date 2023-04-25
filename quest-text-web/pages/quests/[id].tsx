2d5a8123efea3d9c4eb8ca1cea5388bb6f380183
Nyctef <nyctef@nyctef.com>
2023-04-25 13:22:30 +0100

    run prettier, work around type errors for now

--- quest-text-web/pages/quests/[id].tsx
+++ quest-text-web/pages/quests/[id].tsx
@@ -1,30 +1,54 @@
-import { useRouter } from "next/router";
 import React from "react";
 
-function Quest({quest}) {
-  return <div>
-  <h2>{quest.NameJa} / {quest.NameEn}</h2>
-  {/* TODO: add quest names to links */}
-  <p>Next quests: {quest.NextQuests.map((q) => <a href={`/quests/${q}`}>{q}</a>)}</p>
-  <p>Previous quests: {quest.PreviousQuests.map((q) => <a href={`/quests/${q}`}>{q}</a>)}</p>
-  <ul>
-    {quest.TodosEn.map((qt) => <li>{qt}</li>)}
-  </ul>
-  <ul>
-    {quest.JournalEn.map((qt) => <li>{qt}</li>)}
-  </ul>
-  <ul>
-    {quest.DialogueEn.map((qt) => <li>{qt.Speaker}: {qt.Text}</li>)}
-  </ul>
-  </div>;
+function Quest({ quest }: { quest: any }) {
+  return (
+    <div>
+      <h2>
+        {quest.NameJa} / {quest.NameEn}
+      </h2>
+      {/* TODO: add quest names to links */}
+      <p>
+        Next quests:{" "}
+        {quest.NextQuests?.map((q: any) => (
+          <a href={`/quests/${q}`}>{q}</a>
+        ))}
+      </p>
+      <p>
+        Previous quests:{" "}
+        {quest.PreviousQuests.map((q: any) => (
+          <a href={`/quests/${q}`}>{q}</a>
+        ))}
+      </p>
+      <ul>
+        {quest.TodosEn.map((qt: any) => (
+          <li>{qt}</li>
+        ))}
+      </ul>
+      <ul>
+        {quest.JournalEn.map((qt: any) => (
+          <li>{qt}</li>
+        ))}
+      </ul>
+      <ul>
+        {quest.DialogueEn.map((qt: any) => (
+          <li>
+            {qt.Speaker}: {qt.Text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function getStaticPaths() {
   const data = require("../../public/all-quest-texts.data.json");
-  return { paths: Object.keys(data).map((id) => ({ params: { id } })), fallback: false };
+  return {
+    paths: Object.keys(data).map((id) => ({ params: { id } })),
+    fallback: false,
+  };
 }
 
-function getStaticProps({ params }) {
+function getStaticProps({ params} : { params: any }) {
   const data = require("../../public/all-quest-texts.data.json");
   return {
     props: {
