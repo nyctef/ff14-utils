b0c95ffcde5863789216d8bab398b5f536151d40
Nyctef <nyctef@nyctef.com>
2023-12-02 20:25:03 +0000

    integration test for a crafting sequence where teamcraft says we fail

--- crafting-simulator/tests/example_crafts.rs
+++ crafting-simulator/tests/example_crafts.rs
@@ -39,3 +39,58 @@ fn can_craft_garnet_cotton_with_100_percent_quality() {
     assert_eq!(CraftStatus::Success, report.status);
     assert!(report.final_state.quality >= recipe.quality_target);
 }
+
+#[test]
+fn why_doesnt_this_work() {
+    let recipe = p::rlvl640_gear();
+    let player = p::l90_player_with_jhinga_biryani_hq_and_draught();
+    let report = s::run_steps(
+        player,
+        recipe,
+        &[
+            "Prudent Touch",
+            "Basic Touch",
+            "Standard Touch",
+            "Manipulation",
+            "Basic Touch",
+            "Standard Touch",
+            "Muscle Memory",
+            "Veneration",
+            "Basic Synthesis",
+            "Waste Not II",
+            "Groundwork",
+            "Prudent Synthesis",
+            "Careful Synthesis",
+            "Veneration",
+            "Groundwork",
+            "Manipulation",
+            "Basic Synthesis",
+            "Groundwork",
+            "Basic Touch",
+            "Innovation",
+            "Great Strides",
+            "Basic Touch",
+            "Standard Touch",
+            "Byregot's Blessing",
+            "Careful Synthesis",
+            "Muscle Memory",
+            "Muscle Memory",
+            "Byregot's Blessing",
+            "Byregot's Blessing",
+            "Groundwork",
+            "Basic Touch",
+            "Standard Touch",
+            "Advanced Touch",
+            "Standard Touch",
+            "Byregot's Blessing",
+            "Basic Touch",
+            "Standard Touch",
+            "Manipulation",
+            "Careful Synthesis",
+            "Veneration",
+        ],
+    );
+
+    dbg!(&report);
+    assert_eq!(CraftStatus::Failure, report.status);
+}
