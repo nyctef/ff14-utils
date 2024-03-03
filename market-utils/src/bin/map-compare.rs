85c7c0a439bbb24395af6f150e133778ed8274df
Nyctef <nyctef@nyctef.com>
2024-03-03 10:03:17 +0000

    new cli for checking different timeworn map prices

---
+++ market-utils/src/bin/map-compare.rs
@@ -0,0 +1,44 @@
+use color_eyre::eyre::Result;
+use ff14_data::{lookup::ItemLookup, model::Item};
+use ff14_utils::universalis::get_market_data_lookup;
+use itertools::Itertools;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    run().await
+}
+
+async fn run() -> Result<()> {
+    let lookup = ItemLookup::from_datamining_csv().await?;
+    let maps = lookup
+        .matching(|i| i.name.starts_with("Timeworn") && i.name.ends_with("Map"))
+        .collect_vec();
+
+    // dbg!(&maps);
+    //
+    let ids = maps.iter().map(|l| l.id).collect_vec();
+
+    let market_data = get_market_data_lookup(&ids).await?;
+
+    let mut bottom_lines = maps
+        .iter()
+        .map(|map| -> Result<(u32, &Item)> {
+            let market_price = market_data
+                .get(&map.id)
+                .and_then(|md| md.listings.first().map(|l| l.price_per_item))
+                .unwrap_or(0);
+            Ok((market_price, map))
+        })
+        .filter_map(|r| r.ok())
+        .collect_vec();
+
+    bottom_lines.sort_by_key(|l| l.0);
+
+    for line in bottom_lines {
+        println!("{} - {}", line.0, line.1.name);
+    }
+
+    Ok(())
+}
