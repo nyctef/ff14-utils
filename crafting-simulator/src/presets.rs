cb24642e9e3339912f3dbddda1c7021ad701ceab
Nyctef <nyctef@nyctef.com>
2024-03-28 12:11:41 +0000

    remove some presets that are now unused

--- crafting-simulator/src/presets.rs
+++ crafting-simulator/src/presets.rs
@@ -9,22 +9,6 @@ lazy_static! {
 pub struct Presets;
 
 impl Presets {
-    /// pajamas!
-    pub fn l90_chocobo_glam() -> SimulatorRecipe {
-        SimulatorRecipe {
-            rlvl: 610,
-            progress_divider: 130,
-            progress_modifier: 80,
-            quality_divider: 115,
-            quality_modifier: 70,
-            difficulty: 4400,
-            durability: 70,
-            quality_target: 8200,
-            required_craftsmanship: 0,
-            required_control: 0,
-        }
-    }
-
     /// ie diadochos gear
     pub fn l90_4star_gear() -> SimulatorRecipe {
         SimulatorRecipe {
@@ -57,92 +41,8 @@ impl Presets {
         }
     }
 
-    /// eg Indagator's gear
-    pub fn l90_3star_gear() -> SimulatorRecipe {
-        SimulatorRecipe {
-            rlvl: 620,
-            progress_divider: 130,
-            quality_divider: 115,
-            progress_modifier: 80,
-            quality_modifier: 70,
-            difficulty: 5720,
-            durability: 70,
-            quality_target: 12900,
-            required_craftsmanship: 3700,
-            required_control: 3280,
-        }
-    }
-
-    /// eg ilmenite ingot
-    pub fn l90_3star_intermediate() -> SimulatorRecipe {
-        SimulatorRecipe {
-            rlvl: 610,
-            progress_divider: 130,
-            progress_modifier: 80,
-            quality_divider: 115,
-            quality_modifier: 70,
-            difficulty: 3696,
-            durability: 35,
-            quality_target: 8200,
-            required_craftsmanship: 3700,
-            required_control: 3280,
-        }
-    }
-
-    /// for customized components
-    pub fn l90_relic_tier3() -> SimulatorRecipe {
-        SimulatorRecipe {
-            rlvl: 620,
-            progress_divider: 130,
-            quality_divider: 115,
-            progress_modifier: 80,
-            quality_modifier: 70,
-            difficulty: 4620,
-            durability: 60,
-            quality_target: 12040,
-            required_craftsmanship: 0,
-            required_control: 0,
-        }
-    }
-
-    /// for brilliant components
-    pub fn l90_relic_tier4() -> SimulatorRecipe {
-        SimulatorRecipe {
-            rlvl: 625,
-            progress_divider: 130,
-            quality_divider: 115,
-            progress_modifier: 80,
-            quality_modifier: 70,
-            difficulty: 5280,
-            durability: 60,
-            quality_target: 13050,
-            required_craftsmanship: 0,
-            required_control: 0,
-        }
-    }
-
-    /// the max white scrip collectible
-    pub fn l89_collectible() -> SimulatorRecipe {
-        SimulatorRecipe {
-            rlvl: 555,
-            progress_divider: 129,
-            progress_modifier: 100,
-            quality_divider: 113,
-            quality_modifier: 100,
-            difficulty: 3400,
-            durability: 80,
-            quality_target: 7100,
-            required_craftsmanship: 0,
-            required_control: 0,
-        }
-    }
-
     pub fn l90_player() -> PlayerStats {
         PlayerStats::level_90(4014, 3574, 500)
-    }
-
-    pub fn l90_player_2() -> PlayerStats {
-        PlayerStats::level_90(3862, 3529, 576)
     }
 
     pub fn l90_player_with_jhinga_biryani_hq() -> PlayerStats {
