8f9cfa934f42f914a13a1152509bf0c04add3c5d
Nyctef <nyctef@nyctef.com>
2025-04-05 11:42:00 +0100

    more simplification

--- market-utils/src/bin/heliometry-prices.rs
+++ market-utils/src/bin/heliometry-prices.rs
@@ -1,5 +1,6 @@
 use ff14_data::lookup::ItemLookup;
 use ff14_utils::universalis::get_market_data;
+use itertools::Itertools;
 
 #[tokio::main]
 async fn main() {
@@ -16,25 +17,24 @@ async fn main() {
         "Dichromatic Compound",
     ];
 
-    let item_ids: Vec<_> = items
+    let items = items
         .iter()
-        .filter_map(|&name| items_lookup.item_by_name_opt(name).map(|item| item.id))
-        .collect();
+        .filter_map(|&name| items_lookup.item_by_name_opt(name))
+        .collect_vec();
+    let item_ids = items.iter().map(|item| item.id).collect_vec();
 
     let market_data = get_market_data(&item_ids)
         .await
         .expect("Failed to fetch market data");
 
-    for &name in &items {
-        let price = items_lookup.item_by_name_opt(name).and_then(|item| {
-            market_data
-                .iter()
-                .find(|data| data.item_id == item.id)
-                .and_then(|data| data.listings.first().map(|listing| listing.price_per_item))
-        });
+    for &item in &items {
+        let price = market_data
+            .iter()
+            .find(|data| data.item_id == item.id)
+            .and_then(|data| data.listings.first().map(|listing| listing.price_per_item));
         match price {
-            Some(p) => println!("{}: {} gil", name, p),
-            None => println!("{}: Price not available", name),
+            Some(p) => println!("{}: {} gil", item.name, p),
+            None => println!("{}: Price not available", item.name),
         }
     }
 }
