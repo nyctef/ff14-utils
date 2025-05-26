6145b1b9bbd238b41039708b66938cac23444bc3
Nyctef <nyctef@nyctef.com>
2025-05-26 11:04:19 +0100

    dedupe code for printing sell values for scrip items

--- market-utils/src/bin/cosmocredit-items.rs
+++ market-utils/src/bin/cosmocredit-items.rs
@@ -1,17 +1,11 @@
 use color_eyre::eyre::{eyre, Context, Result};
-use ff14_data::lookup::ItemLookup;
-use ff14_utils::universalis::get_market_data;
+use ff14_utils::scrip_compare::print_script_sink_compare;
 use itertools::Itertools;
 use std::env;
-use thousands::Separable;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
-
-    let items_lookup = ItemLookup::from_datamining_csv()
-        .await
-        .expect("Failed to load item data");
 
     let mut target_scrip_count = 10_000;
 
@@ -66,48 +60,7 @@ async fn main() -> Result<()> {
         (900, "Craftsman's Command Materia XII"),
     ];
 
-    let items = items
-        .iter()
-        .filter_map(|(scrip, name)| Some(*scrip).zip(items_lookup.item_by_name_opt(name)))
-        .collect_vec();
-    let item_ids = items.iter().map(|(_, item)| item.id).collect_vec();
-
-    let market_data = get_market_data(&item_ids)
-        .await
-        .expect("Failed to fetch market data");
-
-    let mut results = items
-        .into_iter()
-        .map(|(scrip_cost, item)| {
-            let item_count = target_scrip_count / scrip_cost;
-            let prices = market_data.iter().find(|data| data.item_id == item.id);
-            // let buy_price = prices.and_then(|data| price_up_to(&data.listings, item_count, false).ok());
-            // for the purposes of selling, though, we're going to undercut
-            // whatever the cheapest price currently is
-            let sell_price = prices
-                .and_then(|data| data.listings.first().map(|listing| listing.price_per_item))
-                .map(|p| p * item_count);
-            match sell_price {
-                Some(p) => (
-                    p / target_scrip_count,
-                    format!(
-                        "{}x {}: {} gil (~{} per scrip)",
-                        item_count,
-                        item.name,
-                        p.separate_with_commas(),
-                        (p / target_scrip_count).separate_with_commas()
-                    ),
-                ),
-                None => (0, format!("{}: Price not available", item.name)),
-            }
-        })
-        .collect_vec();
-
-    results.sort_by_key(|(per_scrip, _)| -(*per_scrip as i64));
-
-    for (_, line) in results {
-        println!("{}", line);
-    }
+    print_script_sink_compare(&items, target_scrip_count).await;
 
     Ok(())
 }
--- market-utils/src/scrip_compare.rs
+++ market-utils/src/scrip_compare.rs
@@ -1,5 +1,5 @@
 ﻿use crate::recipe_calculation::process_recipe_item;
-use crate::universalis::get_market_data_lookup;
+use crate::universalis::{get_market_data, get_market_data_lookup};
 use ff14_data::lookup::{ItemLookup, RecipeLookup};
 use ff14_data::model::Recipe;
 use itertools::Itertools;
@@ -64,4 +64,53 @@ pub async fn print_scrip_source_compare(
     }
     // TODO: maybe a --detailed option to print all results?
     Ok(())
+}
+
+pub async fn print_script_sink_compare(items: &[(u32, &'static str)], target_scrip_count: u32) {
+    let items_lookup = ItemLookup::from_datamining_csv()
+        .await
+        .expect("Failed to load item data");
+
+    let items = items
+        .iter()
+        .filter_map(|(scrip, name)| Some(*scrip).zip(items_lookup.item_by_name_opt(name)))
+        .collect_vec();
+    let item_ids = items.iter().map(|(_, item)| item.id).collect_vec();
+
+    let market_data = get_market_data(&item_ids)
+        .await
+        .expect("Failed to fetch market data");
+
+    let mut results = items
+        .into_iter()
+        .map(|(scrip_cost, item)| {
+            let item_count = target_scrip_count / scrip_cost;
+            let prices = market_data.iter().find(|data| data.item_id == item.id);
+            // let buy_price = prices.and_then(|data| price_up_to(&data.listings, item_count, false).ok());
+            // for the purposes of selling, though, we're going to undercut
+            // whatever the cheapest price currently is
+            let sell_price = prices
+                .and_then(|data| data.listings.first().map(|listing| listing.price_per_item))
+                .map(|p| p * item_count);
+            match sell_price {
+                Some(p) => (
+                    p / target_scrip_count,
+                    format!(
+                        "{}x {}: {} gil (~{} per scrip)",
+                        item_count,
+                        item.name,
+                        p.separate_with_commas(),
+                        (p / target_scrip_count).separate_with_commas()
+                    ),
+                ),
+                None => (0, format!("{}: Price not available", item.name)),
+            }
+        })
+        .collect_vec();
+
+    results.sort_by_key(|(per_scrip, _)| -(*per_scrip as i64));
+
+    for (_, line) in results {
+        println!("{}", line);
+    }
 }
