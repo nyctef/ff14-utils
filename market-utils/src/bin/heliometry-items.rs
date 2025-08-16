5e7afef3b793ac3cd1c47c27ec7f417459d1429c
Nyctef <nyctef@nyctef.com>
2025-08-16 10:26:35 +0100

    reuse print_script_sink_compare for heliometry-items

--- market-utils/src/bin/heliometry-items.rs
+++ market-utils/src/bin/heliometry-items.rs
@@ -1,40 +1,21 @@
-use ff14_data::lookup::ItemLookup;
-use ff14_utils::universalis::get_market_data;
-use itertools::Itertools;
+use ff14_utils::scrip_compare::print_script_sink_compare;
 
 #[tokio::main]
 async fn main() {
-    let items_lookup = ItemLookup::from_datamining_csv()
-        .await
-        .expect("Failed to load item data");
-
     let items = [
-        "Hydrophobic Preservative",
-        "Shaaloani Coke",
-        "Neo Abrasive",
-        "Cronopio Skin",
-        "Diatryma Pelt",
-        "Dichromatic Compound",
+        (20, "Hydrophobic Preservative"),
+        (20, "Shaaloani Coke"),
+        (20, "Neo Abrasive"),
+        (20, "Cronopio Skin"),
+        (20, "Diatryma Pelt"),
+        (20, "Dichromatic Compound"),
+        (10, "Potsworn's Abrasive"),
+        (10, "Pelupelu Yarn"),
+        (10, "Purussaurus Skin"),
+        (10, "Xbr'aal Varnish"),
+        (10, "Airbright Coolant"),
+        (10, "Glossy Dried Ether"),
     ];
 
-    let items = items
-        .iter()
-        .filter_map(|&name| items_lookup.item_by_name_opt(name))
-        .collect_vec();
-    let item_ids = items.iter().map(|item| item.id).collect_vec();
-
-    let market_data = get_market_data(&item_ids)
-        .await
-        .expect("Failed to fetch market data");
-
-    for &item in &items {
-        let price = market_data
-            .iter()
-            .find(|data| data.item_id == item.id)
-            .and_then(|data| data.listings.first().map(|listing| listing.price_per_item));
-        match price {
-            Some(p) => println!("{}: {} gil", item.name, p),
-            None => println!("{}: Price not available", item.name),
-        }
-    }
+    print_script_sink_compare(&items, 2000).await;
 }
