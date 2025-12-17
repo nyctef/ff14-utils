3b9319a306d9a2c64ac86df00320e9a0c633b10a
Nyctef <nyctef@nyctef.com>
2025-12-17 20:55:44 +0000

    update some item lists

--- market-utils/src/bin/mathematics-items.rs
+++ market-utils/src/bin/mathematics-items.rs
@@ -3,12 +3,17 @@ use ff14_utils::scrip_compare::print_script_sink_compare;
 #[tokio::main]
 async fn main() {
     let items = [
-        (20, "Hydrophobic Preservative"),
-        (20, "Shaaloani Coke"),
-        (20, "Neo Abrasive"),
-        (20, "Cronopio Skin"),
-        (20, "Diatryma Pelt"),
-        (20, "Dichromatic Compound"),
+        (20, "Turali Pigment"),
+        (20, "Mastodon Pelt"),
+        (20, "Everkeep Resin"),
+        (20, "Insulating Varnish"),
+        (20, "Double Duracoat"),
+        (20, "Yollal Extract"),
+        (10, "Shaaloani Coke"),
+        (10, "Neo Abrasive"),
+        (10, "Cronopio Skin"),
+        (10, "Diatryma Pelt"),
+        (10, "Dichromatic Compound"),
         (10, "Potsworn's Abrasive"),
         (10, "Pelupelu Yarn"),
         (10, "Purussaurus Skin"),
